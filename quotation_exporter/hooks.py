030e3d6b7eba98473c41d7e366988c39506c79d8
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-10 13:48:29 +0700

    export excel

---
+++ quotation_exporter/hooks.py
@@ -0,0 +1,8 @@
+# hooks.py - cấu hình cơ bản cho app
+
+app_name = "quotation_exporter"
+app_title = "Quotation Exporter"
+app_publisher = "Your Name"
+app_description = "Export quotations to Excel"
+app_email = "you@example.com"
+app_license = "MIT"
